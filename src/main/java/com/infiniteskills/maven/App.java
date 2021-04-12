9a095294464c1c66c907944261e7624d8606ed45
KumarMouthikTumuluri <67367625+KumarMouthikTumuluri@users.noreply.github.com>
2021-04-12 10:51:53 +0530

    Update App.java

--- src/main/java/com/infiniteskills/maven/App.java
+++ src/main/java/com/infiniteskills/maven/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        
+         System.out.println( "web-hook" );
+
         System.out.println( "Hello World!" );
     }
 }
